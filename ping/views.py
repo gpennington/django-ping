7589e2d310d705a341d2ed68396a07350996d012
Garrett Pennington <garrettp@gmail.com>
2012-12-10 11:00:00 -0600

    few quick fixes

--- ping/views.py
+++ ping/views.py
@@ -1,15 +1,18 @@
-from django.conf import settings
 from django.http import HttpResponse
+from django.conf import settings
+from django.utils import simplejson
+
+from ping import defaults
 
 def status(request):
     """
     Returns a simple HttpResponse
     """
     
-    response = getattr(settings, 'PING_DEFAULT_RESPONSE', ping.defaults.PING_DEFAULT_RESPONSE)
-    mimetype = getattr(settings, 'PING_DEFAULT_MIMETYPE', ping.defaults.PING_DEFAULT_MIMETYPE)
+    response = getattr(settings, 'PING_DEFAULT_RESPONSE', defaults.PING_DEFAULT_RESPONSE)
+    mimetype = getattr(settings, 'PING_DEFAULT_MIMETYPE', defaults.PING_DEFAULT_MIMETYPE)
     
-    if request.GET.get('json'):
+    if request.GET.get('fmt') == 'json':
         response = simplejson.dumps({ 'up': True })
         mimetype = 'application/json'
         
