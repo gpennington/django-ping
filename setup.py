a4393e567e719feb676dd8bfbf613e279ac47429
Garrett Pennington <garrettp@gmail.com>
2012-12-12 10:48:07 -0600

    setup version number

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-version = '0.1.0'
+version = '0.2.0'
 
 setup(name='django-ping',
       version=version,
       description="Django Monitoring and Availability Utility",
       long_description="",
       classifiers=[
-          "Development Status :: 1 - Planning",
+          "Development Status :: 3 - Alpha",
           "Environment :: Web Environment",
           "Intended Audience :: End Users/Desktop",
           "Natural Language :: English",
