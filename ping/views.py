b5a3c77abef3d6408893f150f2bd4a6dd43d9729
Sebastian Slomski <me@sebslomski.com>
2013-11-11 14:44:01 +0100

    Updated for django 1.6

--- ping/views.py
+++ ping/views.py
@@ -1,6 +1,7 @@
+import json
+
 from django.http import HttpResponse
 from django.conf import settings
-from django.utils import simplejson
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
