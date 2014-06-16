acd09eb08efdf71fde5ee51040e02bc5bacb7d9e
Thomas Schreiber <tom@rizu.fake>
2014-06-16 19:13:46 +0200

    rename mimetype for content_type, django 1.7 compatibility

--- ping/defaults.py
+++ ping/defaults.py
@@ -1,5 +1,5 @@
 PING_DEFAULT_RESPONSE = "Your site is up!"
-PING_DEFAULT_MIMETYPE = 'text/html'
+PING_DEFAULT_CONTENT_TYPE = 'text/html'
 
 PING_DEFAULT_CHECKS = (
     'ping.checks.check_database_sessions',
