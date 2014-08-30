51543dd753fe1cba2f792bcd74dcd04ab162d7b3
Michael Lancaster <michael@michaellancaster.org>
2014-08-30 12:22:47 +1000

    Tweak urls.py to support django 1.6.

--- ping/urls.py
+++ ping/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import patterns, include, url
+try: # django.conf.urls.defaults was removed in django 1.6
+  from django.conf.urls.defaults import patterns, include, url
+except:
+  from django.conf.urls import patterns, include, url
+ 
 from ping.views import status
 
 urlpatterns = patterns('',
